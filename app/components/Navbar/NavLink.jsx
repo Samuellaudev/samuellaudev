d7925e6054c018a79d1403e806df45cbc14f3985
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2023-12-24 13:52:07 +0000

    style: modify navlink style

--- app/components/Navbar/NavLink.jsx
+++ app/components/Navbar/NavLink.jsx
@@ -3,17 +3,20 @@ import clsx from 'clsx';
 
 const NavLink = ({ href, title, pathname }) => {
   return (
-    <Link
-      href={href}
-      className={clsx(
-        'block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white',
-        {
-          'text-white': pathname === href,
-        },
-      )}
-    >
-      {title}
-    </Link>
+    <div className="group">
+      <Link
+        href={href}
+        className={clsx(
+          'block mt-2 py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white',
+          {
+            'text-white': pathname === href,
+          },
+        )}
+      >
+        {title}
+      </Link>
+      <div className="mx-2 mt-0.5 md:mt-2 duration-500 border-b-2 opacity-0 border-white group-hover:opacity-100"></div>
+    </div>
   );
 };
 
