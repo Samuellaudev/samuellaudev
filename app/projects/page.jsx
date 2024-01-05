41be505aca845ad30fd4c1a11158daf8162b7d2d
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2024-01-05 21:14:17 +0000

    chore: update projects page and loading component

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,10 +1,45 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+import { ProjectLoading as Loading } from './Loading';
+import styles from './projectsStyles.module.css';
+
 const Projects = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const numberOfProjects = [{}, {}, {}, {}, {}, {}];
+
+  useEffect(() => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col bg-black">
-      <div className="container mt-2 sm:mt-32 mx-auto py-4 px-12">
-        Projects body
+    <section className="flex min-h-screen flex-col">
+      <div className="container mx-auto px-12 md:mt-20 text-[#565b5f] dark:text-white">
+        <h2
+          className={`${styles.project__heading} mb-10 dark:text-white text-[#ADB7BE]`}
+        >
+          Projects
+        </h2>
+        <div className="relative">
+          {isLoading ? (
+            <div className="grid md:grid-cols-3 gap-4 text-center">
+              {numberOfProjects.map((item, index) => (
+                <Loading key={index} />
+              ))}
+            </div>
+          ) : (
+            <div className="text-center">
+              <h2>The projects are coming soon!</h2>
+            </div>
+          )}
+        </div>
       </div>
-    </main>
+    </section>
   );
 };
 
