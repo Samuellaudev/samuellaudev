89968bb3e83cb09719702431e1f24cc93ad93f5b
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2025-01-18 00:44:29 +0000

    Refactored SliderToggle and fixed not showing correct initial state

--- components/Animation/SliderToggle.jsx
+++ components/Animation/SliderToggle.jsx
@@ -1,59 +1,63 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import { useState } from 'react';
 import { useTheme } from 'next-themes';
 import { FiMoon, FiSun } from 'react-icons/fi';
+import clsx from 'clsx';
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
 
 const TOGGLE_CLASSES =
   'text-sm font-medium flex items-center gap-2 px-3 md:pl-3 md:pr-3.5 py-3 md:py-1.5 transition-colors relative z-10';
 
 const SliderToggle = () => {
-  const [selected, setSelected] = useState('light');
   const { setTheme, resolvedTheme } = useTheme();
 
-  const handleOnClick = (theme) => {
-    setSelected(theme);
-    setTheme(theme);
-  };
+  // resolvedTheme is undefined during the SSR phase and only gets resolved after the client-side JavaScript takes over.
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => setIsMounted(true), []);
+
+  if (!isMounted) {
+    return <Image src="/svg/tube-spinner.svg" alt="" width={50} height={50} />;
+  }
+
+  const themes = [
+    { name: 'light', icon: <FiMoon />, textClass: 'text-slate-300' },
+    { name: 'dark', icon: <FiSun />, textClass: 'text-slate-400' },
+  ];
 
   return (
     <div className="relative flex w-fit items-center rounded-full">
-      {/* Light */}
-      <button
-        className={`${TOGGLE_CLASSES} ${
-          selected === 'light' ? 'text-white' : 'text-slate-300'
-        }`}
-        onClick={() => handleOnClick('light')}
-      >
-        <FiMoon className="relative z-10 text-lg md:text-sm" />
-        <span className="relative z-10">Light</span>
-      </button>
-      {/* Dark */}
-      <button
-        className={`${TOGGLE_CLASSES} ${
-          selected === 'dark' ? 'text-white' : 'text-slate-400'
-        }`}
-        onClick={() => handleOnClick('dark')}
-      >
-        <FiSun className="relative z-10 text-lg md:text-sm" />
-        <span className="relative z-10">Dark</span>
-      </button>
-
-      {/* Layout */}
+      {/* Foreground Text */}
+      {themes.map(({ name, icon, textClass }) => (
+        <button
+          key={name}
+          className={clsx(
+            TOGGLE_CLASSES,
+            resolvedTheme === name ? 'text-white' : textClass,
+          )}
+          onClick={() => setTheme(name)}
+        >
+          {icon}
+          <span>{name.charAt(0).toUpperCase() + name.slice(1)}</span>
+        </button>
+      ))}
+      {/* Background Layout */}
       <div
-        className={`absolute inset-0 z-0 flex ${
-          selected === 'dark' ? 'justify-end' : 'justify-start'
-        }`}
+        className={clsx(
+          'absolute inset-0 z-0 flex',
+          resolvedTheme === 'dark' ? 'justify-end' : 'justify-start',
+        )}
       >
         <motion.span
           layout
           transition={{ type: 'spring', damping: 15, stiffness: 250 }}
-          className={`h-full w-1/2 rounded-full bg-gradient-to-r ${
-            selected === 'light'
+          className={clsx(
+            'h-full w-1/2 rounded-full bg-gradient-to-r',
+            resolvedTheme === 'light'
               ? 'from-secondary-800 to-secondary-300'
-              : 'from-primary-600 to-primary-400'
-          }`}
+              : 'from-primary-600 to-primary-400',
+          )}
         />
       </div>
     </div>
