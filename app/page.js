882d3e0a19da4f4a4890d74ee96139c41eeebe0d
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2025-01-19 18:43:36 +0000

    Added MarqueeDevIcons to Home page

--- app/page.js
+++ app/page.js
@@ -2,12 +2,14 @@ import HeroSection from '@/components/HomePage/HeroSection';
 import AboutSection from '@/components/HomePage/AboutSection';
 import BlogSection from '@/components/HomePage/BlogSection';
 import PortfolioSection from '@/components/HomePage/PortfolioSection';
+import MarqueeDevIcons from '@/components/Animation/MarqueeDevIcons';
 
 export default function Home() {
   return (
     <div className="container sm:mt-32 mx-auto py-4 px-4">
       <HeroSection />
       <AboutSection />
+      <MarqueeDevIcons />
       <BlogSection />
       <PortfolioSection />
     </div>
