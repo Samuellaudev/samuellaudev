6ea94a7f69adc72db76435a1fb8662934a57b556
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2026-02-24 16:33:48 +0000

    Renaming Middleware to Proxy

--- proxy.js
+++ proxy.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { cookies } from 'next/headers';
 
-export async function middleware(req) {
+export async function proxy(req) {
   const cookieStore = cookies();
   const token = cookieStore.get('jwt');
 
